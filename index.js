0a15129b5e8caf11b76d2c3fea22225021c176e3
xcstream <cheng.xu@all-dream.com>
2018-07-26 17:06:32 +0800

    host

--- index.js
+++ index.js
@@ -19,20 +19,21 @@ server.on('close', function(){
     console.log('Server closed');
 });
 
-console.log('start.')
-function create() {
-    for(var i=0;i<100;i++){
-        var client = new net.Socket();
-        client.connect(PORT, '127.0.0.1', function() {
-        });
-        client.on('error',function (err) {
-            console.log('cannot connect at ',count)
-            process.exit()
-        })
-    }
-    console.log('connection count:',count)
-}
-setInterval(create,0)
+// console.log('start.')
+// function create() {
+//     for(var i=0;i<1;i++){
+//         var client = new net.Socket();
+//         client.connect(PORT, '127.0.0.1', function() {
+//         });
+//         client.on('error',function (err) {
+//             console.log(err)
+//             console.log('cannot connect at ',count)
+//             process.exit()
+//         })
+//     }
+//     console.log('connection count:',count)
+// }
+// setInterval(create,0)
 
 
 
