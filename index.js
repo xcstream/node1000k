aea39fe204281864f0462bc21e003c22ff97c053
xcstream <cheng.xu@all-dream.com>
2018-07-26 16:17:11 +0800

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 1926;
+var count = 0
+server = net.createServer(function(sock) {
+
+}).listen(PORT, HOST);
+
+console.log('listen tcp://127.0.0.1:1926')
+server.on('connection',function () {
+    count++
+    console.log('connection count:',count)
+})
+
+server.on('error', function(err){
+    console.log('Server error:', err.message);
+});
+
+server.on('close', function(){
+    console.log('Server closed');
+});
+
+console.log('start.')
+
+for(var i=0;i<1000;i++){
+    var client = new net.Socket();
+    client.connect(PORT, HOST, function() {
+    });
+}
+
+
+
+
