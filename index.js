8ca221dabb432a7657b7d4504970b8b39b87ec9e
xcstream <cheng.xu@all-dream.com>
2018-07-26 17:36:35 +0800

    100port

--- index.js
+++ index.js
@@ -3,27 +3,33 @@ var net = require('net');
 var PORT = 1926;
 var count = 0
 
-server = net.createServer(function(sock) {
-}).listen(PORT);
 
-console.log('listen port 1926')
-server.on('connection',function () {
-    count++
-})
+for (var port = PORT;port <  PORT+100;port++){
+    var server = net.createServer(function(sock){}).listen(port);
+    server.on('connection',function () {
+        count++
+    })
 
-server.on('error', function(err){
-    console.log('Server error:', err.message);
-});
+    server.on('error', function(err){
+        console.log('Server error:', err.message);
+    });
 
-server.on('close', function(){
-    console.log('Server closed');
-});
+    server.on('close', function(){
+        console.log('Server closed');
+    });
+}
+console.log('listen port',PORT,'-',port)
+
+setTimeout(function () {
+    console.log('connections ',count)
+},1000)
 
 console.log('start.')
+
 function create() {
-    for(var i=0;i<1;i++){
+    for(var i=0;i<100;i++){
         var client = new net.Socket();
-        client.connect(PORT, '127.0.0.1', function() {
+        client.connect(PORT+i, '127.0.0.1', function() {
         });
         client.on('error',function (err) {
             console.log(err)
