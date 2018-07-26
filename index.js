0a97c94bd638d694d985aa44f3e3376d2d71df69
xcstream <cheng.xu@all-dream.com>
2018-07-26 18:40:56 +0800

    minify code

--- index.js
+++ index.js
@@ -1,42 +1,7 @@
-
 var net = require('net');
-
-var PORT = 1926;
 var count = 0
-var range = 300
-
-for (var port = PORT;port <  PORT+range;port++){
-    var server = net.createServer(function(sock){}).listen(port);
-    server.on('connection',function () {
-        count++
-    })
-
-    server.on('error', function(err){
-        console.log('Server error:', err.message);
-    });
-
-    server.on('close', function(){
-        console.log('Server closed');
-    });
-}
-console.log('listen port',PORT,'-',port)
-
-function create() {
-    for(var i=0;i<range;i++){
-        var client = new net.Socket();
-        client.connect(PORT+i, '127.0.0.1', function() {
-        });
-        client.on('error',function (err) {
-            console.log(err)
-            console.log('total connections:',count)
-            process.exit()
-        })
-    }
-    console.log('connection count:',count)
-}
-setInterval(create,0)
-
-
-
-
-
+//server
+new Array(300).fill(1).map((_,index)=>index+1926).map(p=>net.createServer().listen(p).on('connection',_=>count++))
+//client
+setInterval(_=>{new Array(300).fill(1).map((_,index)=>index+1926).map(p=>new net.Socket().connect(p,'127.0.0.1')
+    .on('error',function(){process.exit()}))&&console.log('connection count:',count)},10)
